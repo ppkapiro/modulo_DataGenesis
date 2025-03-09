b93a496feaa3f823fa9302d19d1d17830e232fc2
Pepe Capiro <mentalhealthehr@gmail.com>
2025-03-09 14:52:41 -0400

    fix: Normalize line endings to LF

--- src/analyzers/__init__.py
+++ src/analyzers/__init__.py
@@ -1,2 +1,2 @@
-# Este archivo inicializa el paquete `analyzers`, donde se implementarán los módulos de análisis de datos. 
+# Este archivo inicializa el paquete `analyzers`, donde se implementarán los módulos de análisis de datos.
 # Este archivo está intencionalmente dejado en blanco.
